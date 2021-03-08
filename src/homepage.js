8bdd33dfc20dd1e7784529d046313c9795a59159
Hadiiz <hadiziadeh@hotmail.com>
2021-03-08 19:06:05 +0200

    Content load through JS

---
+++ src/homepage.js
@@ -0,0 +1,50 @@
+const load_home_page = function (element) {
+  element.innerHTML = "";
+  const home = document.createElement("div");
+  home.id = "home";
+
+  const home_div = document.createElement("div");
+  home_div.id = "home-div";
+  home.appendChild(home_div);
+
+  const arr = {
+    "Together with you is my favorite place to be.":
+      "あなたのそばが、私のお気に入りの場所です。",
+    "When I look at you, I see the rest of my life in front of my eyes.":
+      "私があなたを見るとき、目の前に私の残りの人生が見えます。",
+  };
+  for (let key in arr) {
+    const description = document.createElement("p");
+    description.className = "description";
+    description.innerHTML = `${key}<br />${arr[key]}`;
+    home_div.appendChild(description);
+  }
+
+  const menu_btn = document.createElement("button");
+  menu_btn.id = "menu-btn";
+  menu_btn.innerHTML = "Menu";
+  home_div.appendChild(menu_btn);
+
+  element.appendChild(home);
+};
+
+export default load_home_page;
+
+/*
+<div id="home">
+        <h2 id="rest-name">Shinkō</h2>
+        <div id="home-div">
+          <p class="description">
+            Together with you is my favorite place to be.
+            <br />
+            あなたのそばが、私のお気に入りの場所です。
+          </p>
+          <p class="description">
+            When I look at you, I see the rest of my life in front of my eyes.
+            <br />
+            私があなたを見るとき、目の前に私の残りの人生が見えます。
+          </p>
+          <button id="menu-btn">Menu</button>
+        </div>
+      </div>
+*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import load_home_page from "./homepage";
 
+const content_div = document.getElementById("content");
+
+load_home_page(content_div);
